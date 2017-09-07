fefa6c1047c1a6b8786c570bdebc4e6d05da806a
Emily Chai <echai93@gmail.com>
2017-09-07 13:59:53 +0100

    type functionality for title timer and date

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
 console.log("Hello World!!");
 import Typed from 'typed.js';
 
-var options = {
-  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-  typeSpeed: 200
-};
+function typeOutTitle() {
+  return new Typed("#unhidden-title", {
+    stringsElement: '#hidden-title',
+    typeSpeed: 150
+  });
+}
 
-var typed = new Typed("#not-hidden", {
-  stringsElement: '#hidden'
-});
+function typeOutTimer() {
+  return new Typed("#unhidden-timer", {
+    stringsElement: '#hidden-timer',
+    typeSpeed: 150
+  });
+}
+
+function typeOutDate() {
+  return new Typed('#unhidden-date', {
+    stringsElement: '#hidden-date',
+    typeSpeed: 150
+  });
+}
+
+setTimeout(typeOutTitle, 2000);
+setTimeout(typeOutTimer, 6000);
+setTimeout(typeOutDate, 10000);
+
+
+function timer() {
+  var countDownDate = new Date("September 22 , 2017, 00:00:00").getTime();
+  var x = setInterval(function() {
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor( distance / (1000 * 60 * 60 * 24) );
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    document.getElementById("timer").innerHTML = days + ":" + hours + ":" + minutes;
+  });
+}
