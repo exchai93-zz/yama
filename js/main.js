bca0a4c992b371cc405f24208cbfb4c5dbbde7d3
Emily Chai <echai93@gmail.com>
2017-09-07 11:53:18 +0100

    test type.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("Hello World!!");
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+  typeSpeed: 200
+};
+
+var typed = new Typed("#not-hidden", {
+  stringsElement: '#hidden'
+});
